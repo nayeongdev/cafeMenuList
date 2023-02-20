b811fb8ebfb4dcd52c416e5d94d5be7a4f9134a1
nayeongdev <nayeongkim.dev@gmail.com>
2023-02-21 01:18:37 +0900

    메뉴 추가하기

---
+++ src/js/index.js
@@ -0,0 +1,79 @@
+// step1 요구사항 - 돔 조작과 이벤트 핸들링으로 메뉴 관리하기
+// TODO 메뉴 추가
+// - [v] 메뉴의 이름을 입력 받고 엔터키를 누르면 메뉴 추가
+// - [v] 메뉴의 이름을 입력 받고 확인 버튼을 누르면 메뉴 추가
+// - [v] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [v] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [v] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [v] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+const $ = (selector) => document.querySelector(selector);
+
+function App() {
+  // form태그는 웹서버에 무언가를 전송하기위해 사용된다.
+  // enter키를 눌렀을 때 자동으로 전송하는 동작을 크롬이 제공해서 웹페이지가 새로고침된다.
+  // form태그가 자동으로 전송되는걸 막아준다. (ENTER키를 눌렀을 때 새로고침 되는걸 막아준다.)
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  const addMenuName = () => {
+    const espressoMenuName = $("#espresso-menu-name").value;
+
+    if (espressoMenuName === "") {
+      alert("메뉴를 입력하세요.");
+      return;
+    }
+
+    const menuItemTemplate = (espressoMenuName) => {
+      return `
+          <li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>`;
+    };
+    $("#espresso-menu-list").insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplate(espressoMenuName)
+    );
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+    $("#espresso-menu-name").value = "";
+  }
+
+  $("#espresso-menu-submit-button").addEventListener("click", () => {
+    addMenuName();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== "Enter") {
+      return;
+    }
+    addMenuName();
+  });
+}
+
+App();
+
+// TODO 메뉴 수정
+// - [] 메뉴의 수정 버튼을 누르면 메뉴 이름을 수정하는 모달창이 뜬다.
+//   (수정시, 브라우저에서 제공하는 `prompt` 인터페이스를 활용한다.)
+// - [] 모달창에서 새로운 메뉴명을 입력 받고, 확인버튼을 누르면 메뉴 수정
+
+// TODO 메뉴 삭제
+// - [] 메뉴 삭제 버튼을 누르면, 메뉴 삭제 컨펌 모달창이 뜬다.
+//   (삭제시, 브라우저에서 제공하는 `confirm` 인터페이스를 활용한다.)
+// - [] 확인 버튼을 누르면 메뉴 삭제
+// - [] 총 메뉴 갯수를 count하여 상단에 보여준다.
+
